6268a249b9d206443da657064a4f69b812af1d79
Nick Nocella <nicholas.nocella@gmail.com>
2015-08-28 19:25:31 -0600

    Started btree - moving to desktop

--- Nocella_Assignment1.py
+++ Nocella_Assignment1.py
@@ -9,7 +9,7 @@ import Queue
 
 #implement a basic integer stack
 
-class stack ():
+class stack:
     def __init__(self, maxsize):
         if maxsize <= 0:
             self.maxsize = 128
@@ -40,6 +40,50 @@ class stack ():
 
     def checksize(self):
         print "The size of the stack is %d" %len(self.data)
+
+#implement a binary tree
+
+class node:
+    def __init__(self, integer):
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.intKey = integer
+
+class bTree:
+    def __init__(self,integer):
+        #the way the assignmnet is written it seems you should not be able to instantiate an empty btree unless you want constant parent not found messages :P
+        self.root = node(integer)
+    def search(self,integer):
+        self._search(integer,self.root)
+
+    def _search(self, integer, node):
+        if (integer == node.intKey):
+            return node
+        elif (integer < node.intKey and node.left != None):
+            self._search(integer, node.left)
+        elif (integer > node.intKey and node.right !=None):
+            self._search(integer, node.right)
+
+    def add(self,integer, parent):
+        if self.search(parent) != None:
+             self._add(integer, node)
+        else:
+            print "Parent not found"
+    def _add(self, integer, node):
+        if (integer < node.intKey):
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
